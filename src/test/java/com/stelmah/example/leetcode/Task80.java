0015cdf65451909a68026d36afe2505ac26d4149
Andrei Stselmashok <andrei.stselmashok@free.fi>
2024-04-23 15:47:51 +0200

    Task 80 java

--- src/test/java/com/stelmah/example/leetcode/Task80.java
+++ src/test/java/com/stelmah/example/leetcode/Task80.java
@@ -3,53 +3,42 @@ package com.stelmah.example.leetcode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-
 class Task80 {
 
-	@Test
-	void example1() {
-		var board = new int[] { 1, 1, 1, 2, 2, 3 };
-		var expected = new int[] { 1, 1, 2, 2, 3, 3 };
+    @Test
+    void example1() {
+        var board = new int[]{1, 1, 1, 2, 2, 3};
+        var expected = new int[]{1, 1, 2, 2, 3, 3};
 
-		Assertions.assertEquals(5, removeDuplicates(board));
-		Assertions.assertArrayEquals(expected, board);
-	}
+        Assertions.assertEquals(5, removeDuplicates(board));
+        Assertions.assertArrayEquals(expected, board);
+    }
+    @Test
+    void example2() {
+        var board = new int[]{1, 2, 3, 4, 5, 6};
+        var expected = new int[]{1, 2, 3, 4, 5, 6};
 
-	@Test
-	void example2() {
-		var board = new int[] { 1, 1, 1, 2, 2, 2 };
-		var q = "abc";
-		var q2 = "abc";
+        Assertions.assertEquals(6, removeDuplicates(board));
+        Assertions.assertArrayEquals(expected, board);
+    }
 
-		var board2 = new int[8];
-		var list = new ArrayList<String >(2000);
-		var expected = new int[] { 1, 1, 2, 2, 2, 2 };
-
-		Assertions.assertEquals(4, removeDuplicates(board));
-		Assertions.assertArrayEquals(expected, board);
-	}
-
-	public int removeDuplicates(int[] nums) {
-		var pointer = 0;
-		var current = 1;
-		while (current < nums.length) {
-			if (nums[current] != nums[pointer]) {
-				var size = Math.min(2, current - pointer + 1);
-				for (int i = 0; i < size; i++) {
-					nums[pointer++] = nums[current - 1];
-				}
-//				if (pointer < nums.length) {
-//					nums[pointer] = nums[current];
-//				}
-			}
-			current++;
-		}
-//		if (pointer == )
-
-		return pointer + 1;
-	}
+    /** Beats 100% */
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 3) {
+            return nums.length;
+        }
+        int l = 1;
+        int r = 2;
+        while (r < nums.length) {
+            if (nums[l] != nums[r]) {
+                nums[++l] = nums[r++];
+            } else if (nums[l] == nums[l - 1]) {
+                r++;
+            } else {
+                nums[++l] = nums[r++];
+            }
+        }
+        return l + 1;
+    }
 
 }
