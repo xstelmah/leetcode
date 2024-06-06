f5b80abccd159bc86efcf6caa0f16e27e92b77a5
Andrei Stselmashok <andrei.stselmashok@free.fi>
2024-06-06 16:45:51 +0200

    Task 79, optimization

--- src/test/java/com/stelmah/example/leetcode/Task79.java
+++ src/test/java/com/stelmah/example/leetcode/Task79.java
@@ -5,48 +5,52 @@ import org.junit.jupiter.api.Test;
 
 class Task79 {
 
-	@Test
-	void example1() {
-		var board = new char[][] { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
-		var word = "ABCCED";
-		Assertions.assertEquals(true, exist(board, word));
-	}
+    @Test
+    void example1() {
+        var board = new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+        var word = "ABCCED";
+        Assertions.assertEquals(true, exist(board, word));
+    }
 
-	int[] dx = new int[] { 0, 1, -1, 0 };
-	int[] dy = new int[] { 1, 0, 0, -1 };
+    int[] dx = new int[]{0, 1, -1, 0};
+    int[] dy = new int[]{1, 0, 0, -1};
 
-	public boolean exist(char[][] board, String word) {
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[i].length; j++) {
-				if (board[i][j] == word.charAt(0)) {
-					var chr = board[i][j];
-					board[i][j] = '-';
-					if (rec(board, word.substring(1), j, i)) {
-						return true;
-					}
-					board[i][j] = chr;
-				}
-			}
-		}
-		return false;
-	}
+    public boolean exist(char[][] board, String word) {
+        var chars = word.toCharArray();
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[y].length; x++) {
+                if (board[y][x] == chars[0]) {
+                    if (rec(board, chars, 0, x, y)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
-	public boolean rec(char[][] board, String word, int x, int y) {
-		if (word.length() == 0) {
-			return true;
-		}
-		for (int i = 0; i < 4; i++) {
-			var newX = x + dx[i];
-			var newY = y + dy[i];
-			if (newY >= 0 && newY < board.length && newX >= 0 && newX < board[y].length && board[newY][newX] == word.charAt(0)) {
-				var chr = board[newY][newX];
-				board[newY][newX] = '-';
-				if (rec(board, word.substring(1), newX, newY)) {
-					return true;
-				}
-				board[newY][newX] = chr;
-			}
-		} return false;
-	}
+    public boolean rec(char[][] board, char[] chars, int pos, int x, int y) {
+        if (pos == chars.length - 1) {
+            return true;
+        }
+        var chr = board[y][x];
+        board[y][x] = '.';
+
+        for (int i = 0; i < 4; i++) {
+            var newX = x + dx[i];
+            var newY = y + dy[i];
+            if (newY >= 0 && newY < board.length
+                    && newX >= 0 && newX < board[y].length
+                    && board[newY][newX] == chars[pos + 1]) {
+
+                if (rec(board, chars, pos + 1, newX, newY)) {
+                    return true;
+                }
+            }
+        }
+
+        board[y][x] = chr;
+        return false;
+    }
 
 }
